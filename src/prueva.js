d03573f9edca064705ce01dc6a3ad96c2914836a
reillimeler <bemax7230@gmail.com>
2026-02-03 22:03:48 -0400

    segundo cambio

---
+++ src/prueva.js
@@ -0,0 +1,1 @@
+// cmamdo de prueva
--- test/suma.test.js
+++ test/suma.test.js
@@ -5,4 +5,8 @@ describe('sum', () => {
   it('suma dos números', () => {
     expect(sum(2, 3)).toBe(5);
   });
+ it('sumar dos números enteros', () => {
+    expect(sum(5, 3)).toBe(8);
+  });
 });
+
